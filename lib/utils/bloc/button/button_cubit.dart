9e71d83ae8a4e4e49da1a7e0d8dbd0a4a2117ab4
thien <thien@DESKTOP-INQH0L7>
2024-10-23 20:56:07 +0700

    button

--- lib/utils/bloc/button/button_cubit.dart
+++ lib/utils/bloc/button/button_cubit.dart
@@ -5,20 +5,20 @@ import 'package:star_shop/utils/use_case/use_case.dart';
 class ButtonCubit extends Cubit<ButtonState> {
   ButtonCubit() : super(ButtonInitialState());
 
-  Future<void> execute({dynamic params/*, required UseCase useCase*/}) async {
+  Future<void> execute({dynamic params, required UseCase useCase}) async {
     emit(ButtonLoadingState());
-    // try {
-    //   var data = await useCase.call(params: params);
-    //   data.fold(
-    //     (error) {
-    //       emit(ButtonFailureState(error: error));
-    //     },
-    //     (data) {
-    //       emit(ButtonSuccessState());
-    //     },
-    //   );
-    // } catch (e) {
-    //   emit(ButtonFailureState(error: e.toString()));
-    // }
+    try {
+      var data = await useCase.call(params: params);
+      data.fold(
+        (error) {
+          emit(ButtonFailureState(error: error));
+        },
+        (data) {
+          emit(ButtonSuccessState());
+        },
+      );
+    } catch (e) {
+      emit(ButtonFailureState(error: e.toString()));
+    }
   }
 }
