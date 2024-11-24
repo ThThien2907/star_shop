b189ed0cdaa794e0204e8e4a3cff9774ed4cc990
thien <thien@DESKTOP-INQH0L7>
2024-11-24 17:54:37 +0700

    favorite

---
+++ lib/features/domain/product/use_cases/add_or_remove_favorite_product_use_case.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:star_shop/common/use_case/use_case.dart';
+import 'package:star_shop/features/domain/product/entities/product_entity.dart';
+import 'package:star_shop/features/domain/product/repositories/product_repository.dart';
+import 'package:star_shop/service_locator.dart';
+
+class AddOrRemoveFavoriteProductUseCase extends UseCase<Either, ProductEntity>{
+  @override
+  Future<Either> call({ProductEntity? params}) async {
+    return await sl<ProductRepository>().addOrRemoveFavoriteProduct(params!);
+  }
+}
